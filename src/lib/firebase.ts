780e752ae6811883ef17d53f2c306b91bdb7e020
Muhammad Rafli <rafliadyatma17@gmail.com>
2025-11-29 13:45:05 +0000

    kenapa masih error autentikasi saat login menggunakan google

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import 'dotenv/config';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
